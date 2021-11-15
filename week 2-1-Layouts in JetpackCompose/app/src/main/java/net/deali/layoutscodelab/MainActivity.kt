b857dcd3cb553c2a62715737935bb51cd842f104
younghoon <kimyh8496@deali.net>
2021-11-15 23:27:44 +0900

    2주차 프로젝트 실습(1)

--- week 2-1-Layouts in JetpackCompose/app/src/main/java/net/deali/layoutscodelab/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/net/deali/layoutscodelab/MainActivity.kt
@@ -1,18 +1,29 @@
 package net.deali.layoutscodelab
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.ContentAlpha
-import androidx.compose.material.LocalContentAlpha
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
+import kotlinx.coroutines.launch
 import net.deali.layoutscodelab.ui.theme.LayoutsCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,41 +31,106 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LayoutsCodelabTheme {
-                // A surface container using the 'background' color from the theme
-                PhotographerCard()
+                TwoTextsPreview()
             }
         }
     }
 }
 
 @Composable
-fun PhotographerCard() {
+fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = rememberImagePainter(
+                data = "https://developer.android.com/images/brand/Android_Robot.png"
+            ),
+            contentDescription = "Android Logo",
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(Modifier.width(10.dp))
+        Text("Item #$index", style = MaterialTheme.typography.subtitle1)
+    }
+}
+
+
+@Composable
+fun ScrollingList() {
+    val listSize = 100
+    val scrollState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+
     Column {
-        Text("Alfred Sisley", fontWeight = FontWeight.Bold)
-        // LocalContentAlpha is defining opacity level of its children
-        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
-            Text("3 minutes ago", style = MaterialTheme.typography.body2)
+        Row {
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(0)
+                }
+            }) {
+                Text("Scroll to the top")
+            }
+
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(listSize - 1)
+                }
+            }) {
+                Text("Scroll to the end")
+            }
+        }
+
+        LazyColumn(state = scrollState) {
+            items(listSize) {
+                ImageListItem(it)
+            }
         }
     }
 }
 
-@Preview
 @Composable
-fun PhotographerCardPreview() {
-    LayoutsCodelabTheme {
-        PhotographerCard()
+fun LazyList() {
+    val scrollState = rememberLazyListState()
+    LazyColumn(state = scrollState) {
+        items(100) {
+            Text("Item #$it")
+        }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun SimpleList() {
+    val scrollState = rememberScrollState()
+
+    Column(Modifier.verticalScroll(scrollState)) {
+        repeat(100) {
+            Text("Item #$it")
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    LayoutsCodelabTheme {
-        Greeting("Android")
+fun PhotographerCard() {
+    Row(modifier = Modifier
+        .padding(16.dp)
+        .clip(RoundedCornerShape(4.dp))
+        .clickable {
+            Log.e("kyh!!!", "click")
+        }
+        .padding(16.dp)) {
+        Surface(
+            modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {}
+
+        Column(
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .align(Alignment.CenterVertically)
+        ) {
+            Text("Alfred Sisley", fontWeight = FontWeight.Bold)
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text("3 minutes ago", style = MaterialTheme.typography.body2)
+            }
+        }
     }
 }
